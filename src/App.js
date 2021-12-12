2cf6a8a4c880bc85987f26e993424d3fbeea1f54
onuryuce <onuryuce@jotform.com>
2021-12-12 20:26:44 +0300

    fourth project materials

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
-import NavBar from './NavBar';
-import Body from './Body';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles({
+  lcPage: {
+    width: '500px',
+    minHeight: '400px',
+    margin: '15px auto',
+    background: '#f5e6e0',
+    borderRadius: '15px'
+  },
+  lcPageClicked: {
+    width: '500px',
+    minHeight: '400px',
+    margin: '15px auto',
+    background: '#b2968a',
+    borderRadius: '15px'
+  },
+  body: {
+    width: '100%',
+    minHeight: '800px'
+  },
+  nameTagBody: {
+      marginRight: "auto",
+      marginLeft: "10%",
+      color: 'darkred',
+      fontSize: 40
+  },
+  lcList: {
+      display: 'flex',
+      flexWrap: 'wrap'
+  },
+  imgDiv: {
+    width: '350px',
+    margin: 'auto',
+  },
+  lcPicture: {
+      width: '100%',
+  },
+  tagDiv: {
+    width: '35%',
+    margin: '5% auto',
+    alignItems: 'center'
+  },
+  tag: {
+      margin: 'auto',
+      fontSize: 40,
+      color: 'darkred'
+  }
+});
 
 function App() {
-  const name= 'Onur'
   return (
     <div>
-      <NavBar name={name}/>
-      <Body name={name}/>
     </div>
   );
 }
